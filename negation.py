569a99ab72926955ad049bd8ced13e80b1f2176e
JTris1 <johnathontrisler@gmail.com>
2023-04-20 17:12:44 -0400

    Implemented custom negations following article

--- negation.py
+++ negation.py
@@ -5,12 +5,13 @@ from spacy.matcher import PhraseMatcher
 from spacy.tokens import Span
 from spacy.strings import StringStore
 from negspacy.negation import Negex
+from negspacy.termsets import termset
 
 nlp0 = spacy.load("en_core_sci_sm")
 nlp1 = spacy.load("en_ner_bc5cdr_md")
 
-# clinical_note = "Patient resting in bed. Patient given azithromycin without any difficulty. Patient has audible wheezing, states chest tightness. No evidence of hypertension. Patient denies nausea at this time. zofran declined. Patient is also having intermittent sweating associated with pneumonia. Patient refused pain but tylenol still given. Neither substance abuse nor alcohol use however cocaine once used in the last year. Alcoholism unlikely. Patient has headache and fever. Patient is not diabetic. No signs of diarrhea. Lab reports confirm lymphocytopenia. Cardaic rhythm is Sinus bradycardia. Patient also has a history of cardiac injury. No kidney injury reported. No abnormal rashes or ulcers. Patient might not have liver disease. Confirmed absence of hemoptysis. Although patient has severe pneumonia and fever, test reports are negative for COVID-19 infection. COVID-19 viral infection absent."
-clinical_note = "RECOMMENDATIONS The USPSTF recommends 1-time screening for abdominal aortic aneurysm with ultrasonography in men aged 65 to 75 years who have ever smoked. (B recommendation) The USPSTF recommends that clinicians selectively offer screening for abdominal aortic aneurysm with ultrasonography in men aged 65 to 75 years who have never smoked rather than routinely screening all men in this group."
+clinical_note = "Patient resting in bed. Patient given azithromycin without any difficulty. Patient has audible wheezing, states chest tightness. No evidence of hypertension. Patient denies nausea at this time. zofran declined. Patient is also having intermittent sweating associated with pneumonia. Patient refused pain but tylenol still given. Neither substance abuse nor alcohol use however cocaine once used in the last year. Alcoholism unlikely. Patient has headache and fever. Patient is not diabetic. No signs of diarrhea. Lab reports confirm lymphocytopenia. Cardaic rhythm is Sinus bradycardia. Patient also has a history of cardiac injury. No kidney injury reported. No abnormal rashes or ulcers. Patient might not have liver disease. Confirmed absence of hemoptysis. Although patient has severe pneumonia and fever, test reports are negative for COVID-19 infection. COVID-19 viral infection absent."
+# clinical_note = "RECOMMENDATIONS The USPSTF recommends 1-time screening for abdominal aortic aneurysm with ultrasonography in men aged 65 to 75 years who have ever smoked. (B recommendation) The USPSTF recommends that clinicians selectively offer screening for abdominal aortic aneurysm with ultrasonography in men aged 65 to 75 years who have never smoked rather than routinely screening all men in this group."
 
 # lemmatizing the notes to capture all forms of negation(e.g., deny: denies, denying)
 def lemmatize(note, nlp):
@@ -36,11 +37,30 @@ options = get_entity_options()
 
 
 #adding a new pipeline component to identify negation
+# This works, but assigns things like 'nausea, viral infection, etc' to DISEASE, not NEG_ENTITY
+# See neg_model2 for the fix to this
 def neg_model(nlp_model):
     nlp = spacy.load(nlp_model, disable=['parser'])
     nlp.add_pipe('sentencizer')
     nlp.add_pipe("negex")
     return nlp
+
+def neg_model2(nlp_model):
+    # Add preceding and following negation terms
+    # These get added to the termset 'en_clinical' which is the default for negex
+    ts = termset('en_clinical')
+    ts.add_patterns({
+        "preceding_negations": ['deny', 'refuse', 'neither', 'nor'],
+        "following_negations": ['absent', 'deny', 'decline'],
+    })
+
+    nlp = spacy.load(nlp_model, disable=['parser'])
+    nlp.add_pipe('sentencizer')
+    nlp.add_pipe('negex')
+    print(nlp.get_pipe_meta('negex'))
+
+    return nlp
+
 
 # Negspacy sets a new attribute e._.negex to True if a negative concept is encountered
 
@@ -50,10 +70,10 @@ def negation_handling(nlp_model, note, neg_model):
     note = note.split(".") #sentence tokenization based on delimeter
     note = [n.strip() for n in note] #remove extra spaces at the beg. and end of sentence
     for t in note:
-        print('### t in note loop: \nT: ', t)
+        # print('### t in note loop: \nT: ', t)
 
         doc = nlp(t)
-        print('### t in note loop: \ndoc: ', str(doc))
+        # print('### t in note loop: \ndoc: ', str(doc))
 
         for e in doc.ents:
             rs = str(e._.negex)
@@ -63,7 +83,9 @@ def negation_handling(nlp_model, note, neg_model):
 
 # list of negative concepts from clinical note identified by negspacy
 results0 = negation_handling('en_ner_bc5cdr_md', lem_clinical_note, neg_model)
-# results1 = negation_handling('en_core_sci_sm', lem_clinical_note, neg_model)
+
+# updated list of all the negative concepts from clinical note identified by negspacy
+results1 = negation_handling('en_ner_bc5cdr_md', lem_clinical_note, neg_model2)
 
 
 # identify span objects of matched negative phrases from clinical note
@@ -89,8 +111,14 @@ def overwrite_ent_label(matcher, doc):
 
 matcher = match(nlp1, results0, "NEG_ENTITY")
 
+# Updated negations
+matcher = match(nlp1, results1, "NEG_ENTITY")
+
 # doc0: new doc object with added NEG_ENTITY label
 doc0 = overwrite_ent_label(matcher, doc)
 
+# doc1: new doc object with added custom concepts for "NEG_ENTITY" label
+doc1 = overwrite_ent_label(matcher, doc) 
+
 # visualizing identified Named Entities in clinical input text
-displacy.serve(doc0, style='ent', options=options)
+displacy.serve(doc1, style='ent', options=options)
